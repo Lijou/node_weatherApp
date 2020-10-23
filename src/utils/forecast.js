ee8010c2fb58f0d7dc8122d74dfcea221baee547
Lijou <leo.drobnic@gmail.com>
2020-10-23 22:16:57 +0200

    Added humidity and visibility info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location.", undefined)            
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " out.")
+            console.log(body)
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out and it feels like " + body.current.feelslike + ". Air humidity is " + body.current.humidity + "% and visibility is " + body.current.visibility + "km.")
         }
     })
 }
